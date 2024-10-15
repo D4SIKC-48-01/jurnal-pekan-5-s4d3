b877b42059d5ea3d1040a9339223696bcb932e44
Sari Dewi <dewi.chan@gmail.com>
2024-10-15 11:22:14 +0700

    Update Jurnal05_PR04.java

--- Jurnal05_PR04.java
+++ Jurnal05_PR04.java
@@ -11,9 +11,9 @@ public class Jurnal05_PR04 {
         //System.out.println(x);
         //System.out.println(y);
         
-        for(; ; ){
+        for(int i=1; i<3 ; i++ ){
             System.out.println("Indeks i ke " + i);
-            for(; ; ){
+            for(int j=1; j<=10 ; j++ ){
                 System.out.print(j);
             }
             
